8655793208de1ef3eb8da330f71463c7d6baf5bd
haitrieu123580 <haitrieu123580@gmail.com>
2024-01-31 14:10:46 +0700

    fix: import error

--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,7 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/InnSight.png";
 import rmtdevImg from "@/public/SgroupMember.jpg";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import wordanalyticsImg from "@/public/SgroupMember.jpg";
 
 export const links = [
   {
